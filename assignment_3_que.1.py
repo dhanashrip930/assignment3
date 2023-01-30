441c094bc6b1af05a447e2b4b415220bc7116f2b
dhanashrip930 <121845131+dhanashrip930@users.noreply.github.com>
2023-01-30 20:11:54 +0530

    assignment 3que1 answer1

---
+++ assignment_3_que.1.py
@@ -0,0 +1,14 @@
+#sample List : (8, 2, 3, 0, 7)
+
+#Expected Output : 20
+
+#Explanation:
+#Summation should like 8+2+3+0+7 = 20
+
+
+
+ 
+
+def sum_list(numbers):
+    return sum(numbers)
+print(sum_list([8,2,3,0,7]))
